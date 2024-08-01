865644951f516beeb67fb60222bdd7d05157bc88
Joy <kimjoydev@gmail.com>
2024-08-01 23:47:24 +0900

    Feat: PasswordValidation with RXSwift

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PasswordViewController: UIViewController {
    
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    let descriptionLabel = UILabel()
+    
+    let validText = Observable.just("8자 이상 입력해주세요")
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,12 +26,36 @@ class PasswordViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
-         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        bind()
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(PhoneViewController(), animated: true)
+    func bind() {
+        // 유효성검사결과 text & status라벨과 연결
+        validText
+            .bind(to: descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        // 유효성 검사 조건
+        let validation = passwordTextField
+            .rx.text.orEmpty.map { $0.count >= 8 }
+        
+        // 유효성 검사 결과에 따라 다음 버튼 활성화 & status라벨 숨기기
+        validation
+            .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        // 유효성 검사 결과에 따른 버튼색 결정 연결
+        validation
+            .bind(with: self) { owner, value in
+                let color: UIColor = value ? .systemPink : .lightGray
+                owner.nextButton.backgroundColor = color
+            }.disposed(by: disposeBag)
+        
+        // addTarget 대신 버튼 탭 액션 연결
+        nextButton
+            .rx.tap.bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(PhoneViewController(), animated: true)
+            }.disposed(by: disposeBag)
     }
     
     func configureLayout() {
